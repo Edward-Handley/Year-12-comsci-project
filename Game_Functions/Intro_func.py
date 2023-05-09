5b7936097041a7ee2f8e2f11be700b99b6fead6a
Rangabread <edhandley479@gmail.com>
2023-05-09 18:47:30 +0800

    Re did combat sys
    Re did combat system
    
    Added more rich text

--- Game_Functions/Intro_func.py
+++ Game_Functions/Intro_func.py
@@ -1,35 +1,41 @@
 import random
-
+from rich import print
 from Characters.Characters import Character, Robber
 from Game_Functions.Combat import Combat
 
+def section_Headings(place_holder):
+    print("\n" * 2)
+    print(f"[bold green]{place_holder:^46}[/bold green]")
+    print("[bold green]" + "═" * 46 + "[/bold green]")
 
-def Intro():
-    print("\n \n \n")
-    print("You are a brave knight who is trying to return home from an epic battle")
-    print("You have been travelling for days and are now in a forest \n you must play to your strengths to defeat enemies in three different areas")
-    print("The frightening forest")
-    print("The hostile vilage")
-    print("The trecherous marsh lands")
-    print("Once you do all these you can return home to your kingdom")
-    print("Good luck")
 
 
 
-# def Forrest():
-#     print("You start you journey in a descriptive on a narrow gravel path")
-#     run_or_walk = int(input("You see exit, run [1] or walk [2]"))
-#     if run_or_walk == 1:
-#         print("you begin to run for the exit, your footsteps crunch the gravel....")
-#         print("Just before you reach the exit a robber appears, you must fight him to get throught")
-#         choice = "run"
-#     else:
-#         print("you sneek past")
-#         choice = "walk"
-    
-#     return choice
+def Intro():
+   
+    print("\n"
+      "╔══════════════════════════════════════════╗\n"
+      "║  Your journey will take you through      ║\n"
+      "║  Freaky Forests, Terrifying Towns, and   ║\n"
+      "║  Mysterious Marshlands. You will need to ║\n"
+      "║  make smart decisions and have luck to   ║\n"
+      "║  Win                                     ║\n"
+      "╚══════════════════════════════════════════╝")
+
+
+
+
 
 def Forrest(player):
+    place_holder = "Freaky Forrest"
+    # print("\n"
+    #   "╔══════════════════════════════════════════╗\n"
+    #   "║            The Freaky Forest             ║\n"
+    #   "║                                          ║\n"
+    # )
+    print("\n" * 2)
+    print(f"[bold green]{place_holder:^46}[/bold green]")
+    print("[bold green]" + "═" * 46 + "[/bold green]")
     print("You start your journey in a descriptive on a narrow gravel path")
     run_or_walk = int(input("You see exit, run [1] or walk [2]"))
     if run_or_walk == 1:
@@ -45,3 +51,9 @@ def Forrest(player):
     combat_result = Combat(player, robber)
 
     return choice, combat_result
+
+
+def Town(player):
+    place_holder = "Terrifying Town"
+    print("\n" * 2)
+    section_Headings(place_holder)
--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ import random
 import rich
 
 
-
 class Robber:
     def __init__(self, name, stamina, attack, defense, health):
         self.name = name
@@ -30,82 +29,10 @@ class Robber:
     
 
 
-def Combat(player, enemy):
-    
-        P_attack = player.attack
-        E_attack = enemy.attack
-        P_defense = player.defense
-        E_defense = enemy.defense
-
-        print(f"Combat between {player.name} and {enemy.name}")
-        turn = "p"
-        while player.health > 0 and enemy.health >0:
-            
-            if turn == "p":
-                P_move = int(input("Do you want to attack[1] or defend[2] \n Enter here: "))
-                if P_move == 1:
-                    P_attack_chance = player.attack * random.uniform(0.8, 1.2) > enemy.defense * random.uniform(0.1,1)
-                    if P_attack_chance: #True or False
-                        P_damage = enemy.health
-                        print(f"You hit {enemy.name} for {P_damage} insta killing")
-                        enemy.health -= P_damage
-                    else:
-                        print(f"You missed your attack")
-                elif P_move == 2:
-                    P_defense_chance = player.defense * random.uniform(0.8, 1.2) > enemy.attack * random.uniform(0.1, 1)
-                    if P_defense_chance:
-                        print("You defend the enemies attack")
-                    else:
-                        E_damage = random.randint(10,20)
-                        print(f"You failed to defend their attack and took {E_damage}")
-                        player.health -= E_damage
-                turn = "e"
-            elif turn =="e":
-                print(f"Now it is {enemy.name}'s turn to attack")
-                E_attack_chance = E_attack * random.uniform(0.1,1) > P_attack * random.uniform(0.8,1.2)
-                if E_attack_chance:
-                    E_damage = random.randint(E_attack, player.health)
-                    print(f"{enemy.name} hit you for {E_damage} you have {player.health - E_damage} health left")
-                    player.health -= E_damage
-                else:
-                    print(f"{enemy.name} missed their attack on you")
-        if enemy.health <= 0:
-            print(f"You defeated {enemy.name}")
-            Combat_result = "win"
-        else:
-            print(f"you were defeated")
-            Combat_result = "lose"
-
-                     
-                     
-        
-        
-        
-        
-        
-        
-        
-     
-    
-
-            
-        
-        
-
-
-
-
-
-
 def main():
-    #Begging
-    Pcharacter = Create_Character()
-    #Pstats = Character()
-    #Pcharacter = Character("ed", 80, 80, 80, 80) #just test data
-
     
-   # Pcharacter = Character(name, stamina, attack, defense, health)
-    #Pstats = Pcharacter.get_stats()
+    Pcharacter = Create_Character()
+    
     want_Intro = input("Do you want an intro to the game?   [Y/N]").upper()
     while want_Intro != "Y" and want_Intro != "N":
         print("Invalid entry try again")
@@ -114,16 +41,16 @@ def main():
         Intro()
     else:
         print("No intro, Good luck!")
+    
 
 
-    print(f"Your character {Pcharacter.name} ")
-    enemy = Robber("Robber", 80, 80, 80, 80) #create instance of enemy for forrest section 
-   
-        
-        
+
+    print(f"Your character is Knight{Pcharacter.name} game will now begin....... good luck ")
+  
   
     forrest_result, combat_result = Forrest(Pcharacter)
-    
+    #Combat Result returning "None" need to fix this 
+   
 
 
 
