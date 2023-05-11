32923219d6133e0f0d1c014a18965e1c61dd3ed1
Rangabread <edhandley479@gmail.com>
2023-05-11 18:44:08 +0800

    Add save system
    Added in a player config system
    Also cleaned up all the code

--- Main.py
+++ Main.py
@@ -1,39 +1,87 @@
-from Characters.Characters import *
-from Game_Functions.Intro_func import *
 import random
 import rich
+import time
+import json
+from Characters.Characters import *
+from Game_Functions.Intro_func import *
 
 
-class Robber:
-    def __init__(self, name, stamina, attack, defense, health):
-        self.name = name
-        self.stamina = stamina
-        self.attack = attack
-        self.defense = defense
-        self.health = health
+
+def save_game(player):
+    save_data = {
+        "name": player.name,
+        "stamina": player.stamina,
+        "attack": player.attack,
+        "defense": player.defense,
+        "health": player.health,
+        "inventory": player.inventory  # Save inventory
+    }
+    with open("save_game.json", "w") as save_file:
+        json.dump(save_data, save_file)
+    print("Game saved successfully!")
+
+def load_game():
+    with open("save_game.json", "r") as save_file:
+        save_data = json.load(save_file)
+    loaded_player = Character(
+        save_data["name"],
+        save_data["stamina"],
+        save_data["attack"],
+        save_data["defense"],
+        save_data["health"]
+    )
+    loaded_player.inventory = save_data["inventory"]  # Load inventory
+    print("Game loaded successfully!")
+    return loaded_player
 
 
-    def display_stats(self):
-        print("Character Name: ", self.name)
-        print("Stamina: ", self.stamina)
-        print("Attack: ", self.attack)
-        print("Defense: ", self.defense)
-        print("Health: ", self.health)
+# def main():
 
-    def update_attributes(self, stamina, attack, defense, health): # self.stamina + stamina because otherwise it would override
-        self.stamina =  self.stamina + stamina
-        self.attack = self.attack + attack
-        self.defense = self.defense + defense
-        self.health = self.health + health
+#     Pcharacter = Create_Character()
+
+#     want_Intro = input("Do you want an intro to the game?   [Y/N]").upper()
+#     while want_Intro != "Y" and want_Intro != "N":
+#         print("Invalid entry try again")
+#         want_Intro = input("Do you want an intro to the game?   [Y/N]").upper()
+#     if want_Intro == "Y":
+#         Intro()
+#     else:
+#         print("No intro, Good luck!")
 
     
+#     print(f"Your character is Knight[bold blue] {Pcharacter.name} [/bold blue] game will now begin....... good luck ")
 
+#     forrest_result, combat_result = Forrest(Pcharacter)
+#     time.sleep(3)  # Pause for 3 seconds
+    
+
+#     town_result = Town(Pcharacter)
+#     time.sleep(3)  # Pause for 3 seconds
+
+#     marshland_result = Marshland(Pcharacter)
+
+#     if marshland_result == "win":
+#         time.sleep(3)  # Pause for 3 seconds
+#         Victory()
+
+# main()
 
 def main():
-    
-    Pcharacter = Create_Character()
-    
-    want_Intro = input("Do you want an intro to the game?   [Y/N]").upper()
+    print("Welcome to the game!")
+    print("1. New Game")
+    print("2. Load Game")
+    choice = input("Enter your choice (1/2): ")
+
+    if choice == "1":
+        Pcharacter = Create_Character()
+    elif choice == "2":
+        Pcharacter = load_game()
+    else:
+        print("Invalid choice. Exiting the game.")
+        return
+
+    want_Intro = input("Do you want an intro to the game? [Y/N] ").upper()
+    # ... (rest of the main function)
     while want_Intro != "Y" and want_Intro != "N":
         print("Invalid entry try again")
         want_Intro = input("Do you want an intro to the game?   [Y/N]").upper()
@@ -41,20 +89,28 @@ def main():
         Intro()
     else:
         print("No intro, Good luck!")
+
+    
+    print(f"Your character is Knight[bold blue] {Pcharacter.name} [/bold blue] game will now begin....... good luck ")
+
+    forrest_result, combat_result = Forrest(Pcharacter)
+    time.sleep(3)  # Pause for 3 seconds
     
 
+    town_result = Town(Pcharacter)
+    time.sleep(3)  # Pause for 3 seconds
+
+    marshland_result = Marshland(Pcharacter)
+
+    if marshland_result == "win":
+        time.sleep(3)  # Pause for 3 seconds
+        Victory()
 
 
-    print(f"Your character is Knight{Pcharacter.name} game will now begin....... good luck ")
-  
-  
-    forrest_result, combat_result = Forrest(Pcharacter)
-    #Combat Result returning "None" need to fix this 
-   
-
-
-
-    #Combat(Pcharacter, enemy)
+    # After completing the game, prompt the user to save their progress
+    save_choice = input("Do you want to save your progress? [Y/N] ").upper()
+    if save_choice == "Y":
+        save_game(Pcharacter)
+        print("Your progress has been saved.")
 
 main()
-
